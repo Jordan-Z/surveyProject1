9db96982e86cfbfa03ed2658cd6ade9dcab67295
Jordan-Z <31749271+Jordan-Z@users.noreply.github.com>
2018-02-07 19:48:59 -0600

    Add files via upload
    final project code

--- Survey_Project1.java
+++ Survey_Project1.java
@@ -8,19 +8,20 @@ package Survey_Project1;
 import java.util.*;
 
 /**
- * Class: 44-242 Data Structures Author: (Jordan Zech) Description: (Lab 10)
- * Due: (4/11/2017) I pledge that I have completed the programming assignment
- * independently. I have not copied the code from a student or any source. I
- * have not given my code to any other student. I have not given my code to any
- * other student and will not share this code with anyone under any
- * circumstances.
+ * Class: Survey of Algorithms Author: (Jordan Zech and Jesse Brandt)
+ * Description: (Project 1)
+ *
  */
 public class Survey_Project1 {
+
+    static int ITERS = 0;
+    static int Kvalue = 0;
 
     private static void swapInds(ArrayList<Integer> L, int i, int j) {
         Integer t = L.get(i);
         L.set(i, L.get(j));
         L.set(j, t);
+
     }
 
     public static ArrayList<Integer> insertionSort(ArrayList<Integer> L) {
@@ -36,7 +37,7 @@ public class Survey_Project1 {
         return L;
     }
 
-    public static ArrayList<Integer> insertValueSort(ArrayList<Integer> L, int n) {
+    public static int insertValueSort(ArrayList<Integer> L, int n) {
         for (int i = 0; i < L.size(); i++) {
             int j = i;
             while (j > 0 && L.get(j - 1) > L.get(j)) {
@@ -45,8 +46,8 @@ public class Survey_Project1 {
             }
 
         }
-        System.out.println(L.get(n));
-        return L;
+        // System.out.println(L.get(n));
+        return L.get(n);
     }
 
     public static ArrayList<Integer> selectionSort(ArrayList<Integer> L) {
@@ -107,14 +108,13 @@ public class Survey_Project1 {
     }
 
     private static int mergeKvalue(ArrayList<Integer> A, int n) {
-        System.out.println(A.get(n));
+        // System.out.println(A.get(n));
         return A.get(n);
     }
-    
-    private static ArrayList<Integer> selectAlg(ArrayList<Integer> S)
-    {
+
+    private static int selectAlg(ArrayList<Integer> S, int k) {
         Random rand = new Random();
-        int e = rand.nextInt(20);
+        int e = S.get(rand.nextInt(S.size()));
         ArrayList<Integer> Al = new ArrayList<Integer>();
         ArrayList<Integer> Ae = new ArrayList<Integer>();
         ArrayList<Integer> Ar = new ArrayList<Integer>();
@@ -129,15 +129,26 @@ public class Survey_Project1 {
                 Ar.add(i);
             }
         }
-        selectionSort(Al);
-        selectionSort(Ae);
-        selectionSort(Ar);
-        
-        ArrayList<Integer> sortedA = new ArrayList<Integer>();
-        sortedA.addAll(Al);
-        sortedA.addAll(Ae);
-        sortedA.addAll(Ar);
-        return sortedA;
+        if (k < Al.size()) {
+            return selectAlg(Al, k);
+        } else if (k < (Al.size() + Ae.size())) {
+            return e;
+        } else {
+            return selectAlg(Ar, (k - Al.size() - Ae.size()));
+        }
+        //selectionSort(Al);
+        //selectionSort(Ae);
+        //selectionSort(Ar);
+
+        //ArrayList<Integer> sortedA = new ArrayList<Integer>();
+        //sortedA.addAll(Al);
+        //sortedA.addAll(Ae);
+        //sortedA.addAll(Ar);
+        //return sortedA;
+    }
+
+    public static int KvalueBuiltinSort(ArrayList<Integer> a, int n) {
+        return a.get(n);
     }
 
     public static ArrayList<Integer> randoList(int size) {
@@ -148,10 +159,10 @@ public class Survey_Project1 {
         return C;
     }
 
-    public static double timeInsertion(int size) {
+    public static double timeInsertion(int size, ArrayList<Integer> l) {
         double avg = 0;
-        for (int i = 0; i < 30; i++) {
-            ArrayList<Integer> l = randoList(size);
+        for (int i = 0; i < ITERS; i++) {
+            //ArrayList<Integer> l = randoList(size);
             long start = System.nanoTime();
             insertionSort(l);
             long end = System.nanoTime();
@@ -160,22 +171,22 @@ public class Survey_Project1 {
         return avg;
     }
 
-    public static double timeSelection(int size) {
+    public static double timeSelection(int size, int k, ArrayList<Integer> l) {
         double avg = 0;
-        for (int i = 0; i < 30; i++) {
-            ArrayList<Integer> l = randoList(size);
+        for (int i = 0; i < ITERS; i++) {
+            //ArrayList<Integer> l = randoList(size);
             long start = System.nanoTime();
-            selectAlg(l);
+            selectAlg(l, k);
             long end = System.nanoTime();
             avg += (end - start) / 1000000000.0;
         }
         return avg;
     }
 
-    public static double timeMerge(int size) {
+    public static double timeMerge(int size, ArrayList<Integer> l) {
         double avg = 0;
-        for (int i = 0; i < 30; i++) {
-            ArrayList<Integer> l = randoList(size);
+        for (int i = 0; i < ITERS; i++) {
+
             long start = System.nanoTime();
             mergeSort(l);
             long end = System.nanoTime();
@@ -184,16 +195,23 @@ public class Survey_Project1 {
         return avg;
     }
 
-    public static double timeBuiltinSort(int size) {
+    public static double timeBuiltinSort(int size, ArrayList<Integer> l) {
         double avg = 0;
-        for (int i = 0; i < 30; i++) {
-            ArrayList<Integer> l = randoList(size);
+        for (int i = 0; i < ITERS; i++) {
+
             long start = System.nanoTime();
             Collections.sort(l);
             long end = System.nanoTime();
             avg += (end - start) / 1000000000.0;
         }
         return avg;
+    }
+
+    public static int testKValue(int insertK, int mergeK, int builtinK, int selectK) {
+        if (builtinK == mergeK && builtinK == insertK && builtinK == selectK) {
+            Kvalue++;
+        }
+        return Kvalue;
     }
 
     /**
@@ -204,33 +222,25 @@ public class Survey_Project1 {
         System.out.println("Enter a number (0-9) for k(index in the arraylist): ");
         int n = reader.nextInt();
         //reader.close();
-
-        System.out.print("k value for insertion algorithm ");
-        insertValueSort(randoList(10), n);
-
-        ArrayList<Integer> unsortedList = randoList(10);
-        ArrayList<Integer> mergeList = mergeSort(unsortedList);
-        System.out.print("k value for merge algorithm ");
-        System.out.println(mergeList.get(n));
-
-        ArrayList<Integer> builtinSort = randoList(10);
-        Collections.sort(builtinSort);
-        System.out.print("k value for builtin sorting algorithm ");
-        System.out.println(builtinSort.get(n));
+        System.out.println("Enter a number for ITERS:how many times you want to run loop: ");
+        ITERS = reader.nextInt();
         
-        ArrayList<Integer> selectA = randoList(10);
-        ArrayList<Integer> sortedAlg = new ArrayList<>(selectAlg(selectA));
-        System.out.print("k value for selection algorithm is ");
-        System.out.println(sortedAlg.get(n));
-        
-        int sizes[] = {1, 500, 1000, 5000, 10000, 25000};
+
+        int sizes[] = {100, 500, 1000, 5000, 10000};
         System.out.printf("%-10s %-20s %-20s %-20s %-20s\n", "Size", "Insertion",
                 "Merge", "BuiltinSort", "Selection");
         for (int size : sizes) {
-            System.out.printf("%-10d %-20f %-20f %-20f %-20f\n", size, 
-                    timeInsertion(size), timeMerge(size), timeBuiltinSort(size), 
-                    timeSelection(size));
+            ArrayList<Integer> l = randoList(size);
+            System.out.printf("%-10d %-20f %-20f %-20f %-20f\n", size,
+                    timeInsertion(size, l), timeMerge(size, l), timeBuiltinSort(size, l),
+                    timeSelection(size, n, l));
+            for (int i = 0; i < ITERS; i++) {
+
+                testKValue(insertValueSort(l, n), mergeKvalue(l, n), KvalueBuiltinSort(l, n), selectAlg(l, n));
+            }
+
         }
+        System.out.println("The K value was correct " + Kvalue + " times out of " + ITERS*5);
     }
 
 }
